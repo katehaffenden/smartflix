dfaa4f980d065126609054cd95745b0de6631a98
Kate Haffenden <kate.haffenden@smartpension.co.uk>
2021-06-14 16:08:04 +0100

    Add test for supplied movie params in omdb adapter

--- spec/workers/movie_api_adapter_spec.rb
+++ spec/workers/movie_api_adapter_spec.rb
@@ -3,21 +3,43 @@
 require 'rails_helper'
 
 RSpec.describe OmdbApiAdapter do
-  subject { OmdbApiAdapter.fetch_data }
+  context 'when no title parameter is supplied' do
+    subject { OmdbApiAdapter.fetch_data }
 
-  it 'makes a call to omdb api' do
-    allow(OmdbApiAdapter).to receive(:get_movie_title).and_return('Some+Like+It+Hot')
-    request = stub_request(:get, 'http://www.omdbapi.com/?apikey=9ae4798d&t=Some%20Like%20It%20Hot')
-              .with(
-                headers: {
-                  'Accept' => '*/*',
-                  'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-                  'User-Agent' => 'Ruby'
-                }
-              )
-              .to_return(status: 200, body: '"{Title => Some Like It Hot}"', headers: {})
+    it 'makes a call to omdb api' do
+      allow(OmdbApiAdapter).to receive(:get_movie_title).and_return('Some+Like+It+Hot')
+      request = stub_request(:get, 'http://www.omdbapi.com/?apikey=9ae4798d&t=Some%20Like%20It%20Hot')
+                .with(
+                  headers: {
+                    'Accept' => '*/*',
+                    'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                    'User-Agent' => 'Ruby'
+                  }
+                )
+                .to_return(status: 200, body: '"{Title => Some Like It Hot}"', headers: {})
 
-    subject
-    expect(request).to have_been_requested.once
+      subject
+      expect(request).to have_been_requested.once
+    end
+  end
+
+  context 'when a title parameter is supplied' do
+    subject { OmdbApiAdapter.fetch_data(title) }
+    let(:title) { 'The Godfather' }
+
+    it 'makes a request with the provided movie title params' do
+      request = stub_request(:get, 'http://www.omdbapi.com/?apikey=9ae4798d&t=The%20Godfather')
+                .with(
+                  headers: {
+                    'Accept' => '*/*',
+                    'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                    'User-Agent' => 'Ruby'
+                  }
+                )
+                .to_return(status: 200, body: '"{Title => The Godfather}"', headers: {})
+
+      subject
+      expect(request).to have_been_requested.once
+    end
   end
 end
