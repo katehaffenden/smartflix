329a3f0eeea6454fa73d581d2d37bab3b7e6058f
Kate Haffenden <kate.haffenden@smartpension.co.uk>
2021-06-29 12:09:16 +0100

    Tidy movie_attributes

--- app/workers/create_movie_worker.rb
+++ app/workers/create_movie_worker.rb
@@ -7,14 +7,31 @@ class CreateMovieWorker
   sidekiq_options queue: :movies, retry: false
 
   def perform(title)
-    response = OmdbApiAdapter.fetch_data(title)
-    Movie.create(title: response['Title'],
-                 year: response['Year'].to_i,
-                 rated: response['Rated'],
-                 released: response['Released'].to_datetime,
-                 genre: response['Genre'],
-                 plot: response['Plot'],
-                 runtime: response['Runtime'],
-                 language: response['Language'])
+    fetch_data(title)
+    create_movie(movie_attributes)
+  end
+
+  private
+
+  attr_reader :response
+
+  def create_movie(movie_attributes)
+    Movie.create(title: movie_attributes[:title],
+                 year: movie_attributes[:year].to_i,
+                 rated: movie_attributes[:rated],
+                 released: movie_attributes[:released].to_datetime,
+                 genre: movie_attributes[:genre],
+                 plot: movie_attributes[:plot],
+                 runtime: movie_attributes[:runtime],
+                 language: movie_attributes[:language])
+  end
+
+
+  def fetch_data(title)
+    @response ||= OmdbApiAdapter.fetch_data(title)
+  end
+
+  def movie_attributes
+    @response.transform_keys! { |k| k.downcase.to_sym }
   end
 end
