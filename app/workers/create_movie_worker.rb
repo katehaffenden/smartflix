7d66f09b33c7e006959a1c010c76484d614050fc
Kate Haffenden <kate.haffenden@smartpension.co.uk>
2021-07-23 11:39:28 +0100

    Update movie_worker to use create_movie_unit

--- app/workers/create_movie_worker.rb
+++ app/workers/create_movie_worker.rb
@@ -7,27 +7,12 @@ class CreateMovieWorker
 
   def perform(title)
     response = omdb_adapter.fetch_data(title)
-    create_movie(movie_attributes(response))
+    CreateMovie::EntryPoint.new(response)
   end
 
   private
 
-  def create_movie(movie_attributes)
-    Movie.create(title: movie_attributes[:title],
-                 year: movie_attributes[:year].to_i,
-                 rated: movie_attributes[:rated],
-                 released: movie_attributes[:released].to_datetime,
-                 genre: movie_attributes[:genre],
-                 plot: movie_attributes[:plot],
-                 runtime: movie_attributes[:runtime],
-                 language: movie_attributes[:language])
-  end
-
   def omdb_adapter
     @omdb_adapter ||= Omdb::ApiAdapter.new
-  end
-
-  def movie_attributes(response)
-    response.transform_keys! { |k| k.downcase.to_sym }
   end
 end
--- spec/workers/create_movie_worker_spec.rb
+++ spec/workers/create_movie_worker_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe CreateMovieWorker do
 
     let(:title) { 'Some+Like+It+Hot' }
     let(:fake_omdb_adapter) { instance_double(Omdb::ApiAdapter) }
-    let(:response) do
+    let(:response) {instance_double(HTTParty::Response, body: response_body)}
+    let(:response_body) do
       { 'Title' => 'Some Like It Hot', 'Year' => '1959', 'Rated' => 'Passed',
         'Released' => '19 Mar 1959', 'Runtime' => '121 min',
         'Plot' => 'Plot summary goes here', 'Genre' => 'Comedy' }
@@ -17,13 +18,15 @@ RSpec.describe CreateMovieWorker do
     before do
       allow(subject).to receive(:omdb_adapter).and_return(fake_omdb_adapter)
       allow(fake_omdb_adapter).to receive(:fetch_data).and_return(response)
+      allow(response).to receive(:parsed_response).and_return(response_body)
     end
 
-    it 'saves a Movie object with correct attributes' do
-      expect(subject.perform(title)).to have_attributes(title: 'Some Like It Hot', year: 1959, rated: 'Passed',
-                                                        released: '1959-03-19 00:00:00.000000000 +0000'.to_datetime,
-                                                        runtime: '121 min', plot: 'Plot summary goes here',
-                                                        genre: 'Comedy')
+    it 'instantiates a CreateMovie::EntryPoint object' do
+      expect(subject.perform(title)).to be_kind_of(CreateMovie::EntryPoint)
+    end
+
+    it 'creates a movie' do
+      expect { subject.perform(title) }.to change(Movie, :count).by(1)
     end
   end
 end
