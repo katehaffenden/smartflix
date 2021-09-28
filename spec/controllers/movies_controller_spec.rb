36d3675a318ae07793b0c51bc8debf66469eadb0
Kate Haffenden <kate.haffenden@smartpension.co.uk>
2021-09-28 11:18:20 +0100

    Edit controller specs to make more like integration tests

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -11,14 +11,13 @@ RSpec.describe MoviesController do
 
       it 'returns a movie as json' do
         get :show, params: { title: movie.title }
+
         expect(response).to have_http_status(:ok)
       end
     end
 
     context 'when movie is not in the database' do
       it 'returns an error' do
-        expect(CreateMovieWorker).to receive(:perform_async)
-
         get :show, params: { title: '-1' }
 
         expect(response).to have_http_status(:not_found)
