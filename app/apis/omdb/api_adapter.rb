f8f1e2d35b03038f6850f728e7eda6e6cf0d323a
Kate Haffenden <kate.haffenden@smartpension.co.uk>
2021-10-01 08:33:07 +0100

    Moving endpoint url for adapter to env variable

--- app/apis/omdb/api_adapter.rb
+++ app/apis/omdb/api_adapter.rb
@@ -7,7 +7,7 @@ module Omdb
   class ApiAdapter
     def fetch_data(title = nil)
       title = movie_title if title.nil?
-      HTTParty.get("http://www.omdbapi.com/?t=#{title}&apikey=#{omdb_key}")
+      HTTParty.get("#{base_url}?t=#{title}&apikey=#{omdb_key}")
     end
 
     def movie_title
@@ -19,6 +19,10 @@ module Omdb
 
     def omdb_key
       ENV['OMDB_KEY']
+    end
+
+    def base_url
+      ENV['BASE_URL']
     end
   end
 end
