0ce8e7cff8140063867f654eabbb71ffea9fd7a1
Kate Haffenden <kate.haffenden@smartpension.co.uk>
2021-10-01 15:20:57 +0100

    Fix linting errors

--- app/units/base_action.rb
+++ app/units/base_action.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
-  class BaseAction
-    EXCLUDED_ATTRIBUTES = %i[director actors awards poster country ratings writer type dvd boxoffice production
-                             metascore response imdbrating imdbvotes imdbid website].freeze
-    private_constant :EXCLUDED_ATTRIBUTES
+class BaseAction
+  EXCLUDED_ATTRIBUTES = %i[director actors awards poster country ratings writer type dvd boxoffice production
+                           metascore response imdbrating imdbvotes imdbid website].freeze
+  private_constant :EXCLUDED_ATTRIBUTES
 
-    private
+  private
 
-    def invalid?(response)
-      response.body.include?('False')
-    end
-
-    def log_warning
-      Rails.logger.warn "#{Time.current} Request returned an error in the response"
-    end
-
-    def prepare_movie_attributes(response)
-      response = response.transform_keys! { |k| k.downcase.to_sym }
-      response.except!(*EXCLUDED_ATTRIBUTES)
-    end
+  def invalid?(response)
+    response.body.include?('False')
   end
+
+  def log_warning
+    Rails.logger.warn "#{Time.current} Request returned an error in the response"
+  end
+
+  def prepare_movie_attributes(response)
+    response = response.transform_keys! { |k| k.downcase.to_sym }
+    response.except!(*EXCLUDED_ATTRIBUTES)
+  end
+end
